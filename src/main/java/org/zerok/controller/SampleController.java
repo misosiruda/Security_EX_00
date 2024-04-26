d3979f506f9c5d602c78bdb85f2be9f77db772e8
misosiruda <81164760+misosiruda@users.noreply.github.com>
2024-04-26 11:13:07 +0900

    로그인 권한 설정

---
+++ src/main/java/org/zerok/controller/SampleController.java
@@ -0,0 +1,32 @@
+package org.zerok.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j
+@RequestMapping("/sample")
+@Controller
+public class SampleController {
+
+    // 모든 사용자가 접근 가능한 페이지
+    @GetMapping("/all")
+    public void doAll() {
+        log.info("모든 사용자가 접근 가능");
+    }
+
+    // 회원이상의 권한 소유자만 접근 가능한 페이지
+    @GetMapping("/member")
+    public void doMember() {
+        log.info("로그인 한 회원 혹은 그이상의 권한 소유자 접근 가능");
+    }
+
+    // 관리자만 접근 가능한 페이지
+    @GetMapping("/admin")
+    public void doAdmin() {
+        log.info("관리자 접근 가능");
+    }
+
+}
